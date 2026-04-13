f3c9996b73e16add4ddeabe8ff1271e7099c3a5a
Bách Trần <you@example.com>
2026-04-13 10:47:04 +0700

    Cập nhật app.py và cấu hình gitignore

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ import asyncio
 if __name__ == "__main__":
     load_dotenv()
     api_key = os.getenv("GROQ_API_KEY")
-    orchestrator = GameOrchestrator(db_path="./data/World.db", vector_model_path="", groq_api_key=api_key)
+    orchestrator = GameOrchestrator(db_path="./data/World.db", vector_model_path="all-MiniLM-L6-v2", groq_api_key=api_key)
     asyncio.run(orchestrator.run())
+    
